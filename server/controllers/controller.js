eb5bf7c5d831562aa9e87cfcc1199362bca4d602
James Fijewski <jfijewsk@asu.edu>
2022-01-10 08:47:29 -0600

    Test IP grabbing

---
+++ server/controllers/controller.js
@@ -0,0 +1,38 @@
+const http = require('http');
+
+function getCompanyName(ip) {
+    var result = '';
+
+    // console.log('atempting to call: http://ip-api.com/json/' + ip);
+    // http.get('http://ip-api.com/json/208.73.132.195', (resp) => {
+    
+    http.get(('http://ip-api.com/json/' + ip), (resp) => {
+
+    let data = '';
+    
+    // A chunk of data has been received.
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+    
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+        // console.log('response ' + JSON.parse(data).org);
+        this.result = JSON.parse(data).org;
+        return(result);
+    });
+    
+    }).on("error", (err) => {
+    console.log("Error: " + err.message);
+    });
+    
+
+}
+
+function getClientAddress(req) {
+    return req.ip.split(":").pop();
+};
+
+
+
+module.exports = {getCompanyName, getClientAddress};
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var controller = require('../controllers/controller')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.send('API working');
+   // controller.getCompanyName('208.73.132.195');
+    console.log('result in index= ' + controller.getCompanyName('208.73.132.195'));
+    console.log('Clients IP = ' + controller.getClientAddress(req));
+    res.send('result = ' + controller.getCompanyName('208.73.132.195'));
 
 });
 
