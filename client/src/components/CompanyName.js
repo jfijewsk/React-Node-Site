466e53008f6396b57da6bc0c6b0a3f0376a91315
James Fijewski <jfijewsk@asu.edu>
2022-01-16 10:38:27 -0600

    no message

--- client/src/components/CompanyName.js
+++ client/src/components/CompanyName.js
@@ -13,7 +13,7 @@ export default class CompanyName extends React.Component {
     componentDidMount() {
       fetch(this.state.apiAddress)
       .then(response => response.json())
-      .then(data => this.setState({apiSuccess: data.status, compName: data.org}))
+      .then(data => this.setState({apiSuccess: data.status, compName: data.org, ispName: data.isp}))
 
       .catch(error =>{
         console.log(error);
@@ -22,7 +22,7 @@ export default class CompanyName extends React.Component {
 
       render () {
         console.log("This is what react sees" + JSON.stringify(this.state));
-        if(this.state.apiSuccess == 'fail' || this.state.isp == this.state.org){
+        if(this.state.apiSuccess == 'fail' || this.state.compName == this.state.ispName){
             this.state.compName = 'your company';
         }
 
